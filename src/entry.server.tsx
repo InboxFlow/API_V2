bd30fb922aecd047182412e4505a5faf5f9dc908
Lucas Eduardo Gonçalves <lucasgoncalvesgithub@gmail.com>
2024-11-18 12:04:51 -0300

    chore(deps): update dependencies and add remix-development-tools
    - Update @arkyn packages to 1.3.153
    - Update @prisma/client and prisma to 5.22.0
    - Update @remix-run packages to 2.14.0
    - Update @wyw-in-js/vite to 0.5.5
    - Update vite to 5.4.11
    - Add remix-development-tools 4.7.6
    - Enable v3_lazyRouteDiscovery and v3_singleFetch in Remix future config
    - Remove unused imports in entry.server.tsx

--- src/entry.server.tsx
+++ src/entry.server.tsx
@@ -6,12 +6,7 @@
 
 import { PassThrough } from "node:stream";
 
-import type {
-  ActionFunctionArgs,
-  AppLoadContext,
-  EntryContext,
-  LoaderFunctionArgs,
-} from "@remix-run/node";
+import type { AppLoadContext, EntryContext } from "@remix-run/node";
 import { createReadableStreamFromReadable } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import { isbot } from "isbot";
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { config } from "dotenv";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { remixDevTools } from "remix-development-tools";
 
 import wyw from "@wyw-in-js/vite";
 
@@ -10,9 +11,12 @@ config({ path: ".env" });
 export default defineConfig({
   plugins: [
     wyw(),
+    remixDevTools(),
     remix({
       appDirectory: "./src",
       future: {
+        v3_lazyRouteDiscovery: true,
+        v3_singleFetch: true,
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
