7b576ce610bd79a22b71dcdcc96e901ef1a6aeda
Lucas Eduardo Gonçalves <lucasgoncalvesgithub@gmail.com>
2024-11-25 14:45:53 -0300

    feat(createCall): add channel validation to CreateCallUsecase
    - Add ChannelRepository to CreateCallUsecase constructor
    - Implement channel existence check before creating a call
    - Update CreateCallUsecase instantiation in index.ts
    - Remove .DS_Store file from src/client/components

--- src/app/usecases/call/createCall/CreateCallUsecase.ts
+++ src/app/usecases/call/createCall/CreateCallUsecase.ts
@@ -1,15 +1,24 @@
+import { BadRequestError } from "@arkyn/server";
 import { Call } from "~/app/entities";
-import { CallRepository } from "~/app/repositories";
+import { CallRepository, ChannelRepository } from "~/app/repositories";
 import { ValidatorAdapter } from "~/infra/adapters";
 import { createCallSchema } from "~/infra/schemas/callSchemas";
 
 class CreateCallUsecase {
-  constructor(private callRepository: CallRepository) {}
+  constructor(
+    private callRepository: CallRepository,
+    private channelRepository: ChannelRepository
+  ) {}
 
   async execute(body: any) {
     const validator = new ValidatorAdapter(createCallSchema);
     const { channelId, method, request, response, token, status } =
       validator.formValidate(body);
+
+    const channel = await this.channelRepository.findById(channelId);
+    if (!channel) throw new BadRequestError("Channel not found");
+
+    console.log(channel);
 
     const call = Call.create({
       status,
--- src/app/usecases/call/createCall/index.ts
+++ src/app/usecases/call/createCall/index.ts
@@ -1,9 +1,13 @@
-import { CallRepository } from "~/app/repositories";
+import { CallRepository, ChannelRepository } from "~/app/repositories";
 import { CreateCallController } from "./CreateCallController";
 import { CreateCallUsecase } from "./CreateCallUsecase";
 
 const callRepository = new CallRepository();
-const createCallUsecase = new CreateCallUsecase(callRepository);
+const channelRepository = new ChannelRepository();
+const createCallUsecase = new CreateCallUsecase(
+  callRepository,
+  channelRepository
+);
 const createCallController = new CreateCallController(createCallUsecase);
 
 const createCall = createCallController;
