1de6a4b31b4d764521f6b3012946f1cf9a18ad1c
Lucas Eduardo Gonçalves <lucasgoncalvesgithub@gmail.com>
2024-12-09 14:21:35 -0300

    chore(deps): update dependencies and improve authentication
    - Update @arkyn packages to version 1.4.8
    - Update Remix packages to version 2.15.1
    - Update various dev dependencies
    - Enhance isAuthenticated middleware to handle API and non-API requests

--- src/main/middlewares/isAuthenticated.ts
+++ src/main/middlewares/isAuthenticated.ts
@@ -1,16 +1,27 @@
 import { BadRequestError, NotFoundError } from "@arkyn/server";
+import { redirect } from "@remix-run/server-runtime";
+
 import { UserRepository } from "~/app/repositories";
 import { JwtAdapter } from "~/infra/adapters";
 import { ContextType } from "~/main/types";
+
 import { authStorage } from "../storages";
 
 async function isAuthenticated(request: ContextType["request"]) {
   const { getSession } = authStorage;
   const session = await getSession(request.headers.get("Cookie"));
 
+  const isAPI =
+    request.headers.get("X-Requested-With") === "XMLHttpRequest" ||
+    request.headers.get("Accept")?.includes("application/json") ||
+    request.headers.get("User-Agent")?.includes("Postman") ||
+    request.headers.get("Content-Type")?.includes("application/json");
+
   const user = session.get("user");
   let token = user?.token || request.headers.get("authorization");
-  if (!token) throw new BadRequestError("Token not sent");
+
+  if (!token && isAPI) throw new BadRequestError("Token not sent");
+  if (!token && !isAPI) throw redirect("/client/auth");
 
   token = token.replace("Bearer ", "").replaceAll(" ", "");
 
