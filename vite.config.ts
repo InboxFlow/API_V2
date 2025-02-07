0a1eae1c7c20777bee1abc5595482ed7866e8caa
Lucas Eduardo Gonçalves <lucasgoncalvesgithub@gmail.com>
2025-02-07 16:04:34 -0300

    chore(deps): upgrade @arkyn packages and add new dependencies
    - Upgrade @arkyn/* packages from 1.4.20 to 1.4.22
    - Add remix-development-tools and vite-env-only as dependencies
    - Update vite.config.ts to include remixDevTools and denyImports
    - Remove some future flags from remix configuration
    - Update bun.lockb file

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,29 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
 import { config } from "dotenv";
+import { remixDevTools } from "remix-development-tools";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 import wyw from "@wyw-in-js/vite";
+import { denyImports } from "vite-env-only";
+import tsconfigPaths from "vite-tsconfig-paths";
 
+installGlobals();
 config({ path: ".env" });
 
 export default defineConfig({
   plugins: [
+    remixDevTools(),
     wyw(),
     remix({
       appDirectory: "./src",
       future: {
-        v3_lazyRouteDiscovery: true,
-        v3_singleFetch: true,
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
       },
     }),
+    denyImports({ client: { specifiers: ["@arkyn/server", "@remix/node"] } }),
     tsconfigPaths(),
   ],
 });
